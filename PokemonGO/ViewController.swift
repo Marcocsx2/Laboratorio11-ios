b8cf8b09c765fedd8d65361b653a89f7b3100af6
Marco Vizcarra <marco.vizcarra@tecsup.edu.pe>
2020-12-10 08:28:50 -0800

    Initial Commit

---
+++ PokemonGO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonGO
+//
+//  Created by Marco Vizcarra on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
